9796c7295f19e5c1f0fa52932bbc9a47037e9d1b
Gang Wen <gang.wen@ingka.ikea.com>
2021-02-08 13:18:33 +0100

    Generated a new project
    Generated a new project

---
+++ 4-View-Controllers/Samples/WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Gang Wen on 2021-02-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
